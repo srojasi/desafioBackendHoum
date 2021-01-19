c0e8451e3ddbfa7698937040941a4f9d83d19dbc
Santiago Rojas <santiago.rojas@ug.uchile.cl>
2021-01-19 11:52:57 -0300

    improved performance in question 3

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import sys
 import aiohttp
 import asyncio
 
+
 def main():
     print("Desafio Backend Houm")
-    #print("Respuesta 1.")
-    #print(pregunta1())
-    #print("Respuesta 2. Version Secuencial")
-    #print(pregunta2())
-    print("Respuesta 2. Version Asincrona")
+    print("Respuesta 1.")
+    print(pregunta1())
+    print("Respuesta 2. Version Secuencial")
+    print(pregunta2())
+    print("Respuesta 2. Versión Asincrona")
     print(pregunta2_async())
-    #print("Respuesta 3.")
-    #print(pregunta3())
+    print("Respuesta 3. Versión Secuencial")
+    print(pregunta3())
+    print("Respuesta 3. Versión Asincrona")
+    print(pregunta3_async())
+
 
 def pregunta1():
     """
@@ -73,6 +77,7 @@ def pregunta2():
     finally:
         return len(arr)
 
+
 def pregunta2_async():
     """
     Answer to question 2 of Houm Challenge, asynchronous version.
@@ -92,22 +97,31 @@ def pregunta2_async():
         response_raichu.raise_for_status()
 
         async def get_species_from_egg_group(session, url):
+            """
+            Async function to get the species from a specific egg_group
+            """
             async with session.get(url) as response:
                 wait_response = await response.json()
                 return wait_response["pokemon_species"]
             
         async def get_all_species(egg_groups):
+            """
+            Async function to get all the species from the array of egg_groups
+            """
             async with aiohttp.ClientSession() as session:
+                # async tasks to be performed
                 tasks = []
                 for egg_group in egg_groups:
                     async_response = get_species_from_egg_group(session, egg_group["url"])
                     tasks.append(async_response)
                 
+                #gather all the tasks
                 responses = await asyncio.gather(*tasks, return_exceptions=True)
                 return responses
         
-        async_get_response = get_all_species(response_raichu.json()["egg_groups"])
-        all_species_by_egg_group = asyncio.run(async_get_response)
+        async_response = get_all_species(response_raichu.json()["egg_groups"])
+        #run coroutines concurrently
+        all_species_by_egg_group = asyncio.run(async_response)
 
         #responses are bundled in a matrix, each row is a egg_group species
         for egg_group_species in all_species_by_egg_group:
@@ -122,35 +136,83 @@ def pregunta2_async():
 
 def pregunta3():
     """
-    Funcion para responder a la pregunta 3 del desafio backend de Houm.
+    Answer to question 3 of Houm Challenge, secuential version.
 
-    Para obtener el maximo y el minimo, se iterara una sola vez por el listado pokemones
-    ya filtrados por generacion, reduciendo la cantidad de consultas a la API.
-    Se puede obtener la id del pokemon desde la url de este.
+    To get the maximum and minimum, it'll loop through the list of pokemon already
+    filtered by generation, reducing the number of calls to the API.
+    The id of a pokemon can be obtain from the url.
     """
-    arr = [0, sys.maxsize]
-    responseFighting = requests.get("https://pokeapi.co/api/v2/type/fighting")
-    if not responseFighting:
-        print('Request fallido')
+    try: 
+        arr = [0, sys.maxsize]
+        response_fighting = requests.get("https://pokeapi.co/api/v2/type/fighting")
+        response_fighting.raise_for_status()
 
-    for pokemon in responseFighting.json()["pokemon"]:
-        if get_id_from_URL(pokemon["pokemon"]["url"]) <= 151:
-            response = requests.get(pokemon["pokemon"]["url"])
-            if response:
-                peso = response.json()["weight"]
-                if arr[0] < peso:
-                    arr[0] = peso
-                if arr[1] > peso:
-                    arr[1] = peso
-            else:
-                print('Request fallido')
+        for pokemon in response_fighting.json()["pokemon"]:
+            if get_id_from_URL(pokemon["pokemon"]["url"]) <= 151:
+                response = requests.get(pokemon["pokemon"]["url"])
+                response.raise_for_status()
 
-    return arr
+                weight = response.json()["weight"]
+                if arr[0] < weight:
+                    arr[0] = weight
+                if arr[1] > weight:
+                    arr[1] = weight
+    except Exception:
+        print("Request fallido")
+    finally:
+        return arr
 
-#Funciones Auxiliares
+
+def pregunta3_async():
+    """
+    Answer to question 3 of Houm Challenge, asynchronical version.
+
+    To get the maximum and minimum, it'll loop through the list of pokemon already
+    filtered by generation, reducing the number of calls to the API. Each of this 
+    calls to the API are made asynchronically for better performance.
+
+    The id of a pokemon can be obtain from the url.
+    """
+    try:
+        response_fighting = requests.get("https://pokeapi.co/api/v2/type/fighting")
+        response_fighting.raise_for_status()
+
+        async def get_pokemon(session, url):
+            """
+            Async function to get a pokemon from a url
+            """
+            async with session.get(url) as response:
+                wait_response = await response.json()
+                return wait_response["weight"]
+        
+        async def get_all_pokemon(pokemons):
+            """
+            Async function to gather all pokemons(<=151)
+            """
+            async with aiohttp.ClientSession() as session:
+                #async tasks to be perfomed
+                tasks = []
+                for pokemon in pokemons:
+                    if get_id_from_URL(pokemon["pokemon"]["url"]) <= 151:
+                        tasks.append(get_pokemon(session, pokemon["pokemon"]["url"]))
+                
+                #gather all tasks
+                responses = await asyncio.gather(*tasks, return_exceptions=True)
+                return responses
+        
+        async_response = get_all_pokemon(response_fighting.json()["pokemon"])
+        all_pokemons = asyncio.run(async_response)
+    except Exception:
+        print("Request fallido")
+    finally:
+        return [max(all_pokemons),min(all_pokemons)]
+
+
+#Auxiliary Functions
 def get_id_from_URL(url):
     arr = url.split("/")
     return int(arr[-2])
 
-main()
 
+
+main()
